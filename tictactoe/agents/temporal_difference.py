70a4b3dff0f2a387e792487974fb2f990ba41e2b
Manuel Meraz-Rodriguez <meraz.manuel@gmail.com>
2019-10-02 20:26:16 -0700

    Add documentation and type hints to temporal difference class

--- tictactoe/agents/temporal_difference.py
+++ tictactoe/agents/temporal_difference.py
@@ -2,42 +2,69 @@
 import pprint
 import random
 from collections import defaultdict
-from typing import Tuple, List
+from typing import Tuple, List, Any
 
-import pandas as pd
+import pandas
 
 from env import Mark
 from .agent import Agent
 
 
 class State:
-    def __init__(self, value=0.0, count=0.0):
+    def __init__(self, value: float = 0.0, count: int = 0):
+        """
+        Stores the value of a state and how many times the agent has been in this state
+        :param value: The total accumulated reward computed using temporal difference
+        :param count:  The number of times the agent has been in this state
+        """
         self.value = value
         self.count = count
 
-    def __str__(self):
+    def __str__(self) -> str:
         return pprint.pformat({"value": self.value, "count": self.count})
 
     def __repr__(self):
         return self.__str__()
 
     def __add__(self, other):
-        total_value = self.value + other.value
-        total_count = self.count + other.count
+        total_value: float = self.value + other.value
+        total_count: int = self.count + other.count
         return State(total_value, total_count)
 
 
 class TemporalDifference(Agent):
-    def __init__(self, exploratory_rate: float = 0.5, learning_rate: float = 0.5, state_values=defaultdict(State)):
+
+    def __init__(self, exploratory_rate: float = 0.1, learning_rate: float = 0.5, state_values=None):
+        """
+        Represents an agent learning with temporal difference
+        :param exploratory_rate: The probability of selecting a random action
+        :param learning_rate: How much to learn from the most recent action
+        :param state_values: A mapping of states to and their associated values
+        """
         self.datafile: str = f"{TemporalDifference.__name__}_{exploratory_rate}_{learning_rate}"
-        self.state_values = state_values
+        if not state_values:
+            self.state_values = TemporalDifference.load_state_values(self.datafile)
+        else:
+            self.state_values = state_values
+
         self.learning_rate: float = learning_rate
         self.exploratory_rate: float = exploratory_rate
 
-    def act(self, state):
+    def act(self, state: Tuple[Mark]) -> int:
+        """
+        Apply this agents policy and select an action
+        :param state:  The current state of the board along with the current mark this agent represents
+        :return: an action
+        """
         return self.egreedy_policy(state, super().available_actions(state))
 
     def egreedy_policy(self, state: Tuple[Mark], available_actions: List[int]) -> int:
+        """
+        Select action based off a probability of exploring or greedy selection of valuable actions
+        :param state:  The current state of the board along with the current mark this agent represents
+        :param available_actions: A list of available possible actions (positions on the board to mark)
+        :return: A greedy action or random action to explore
+        """
         e: float = random.random()
 
         if e < self.exploratory_rate:
@@ -47,24 +74,33 @@ class TemporalDifference(Agent):
 
         return action
 
-    def greedy_action(self, state, available_actions):
+    def greedy_action(self, state: Tuple[Mark], available_actions: List[int]) -> int:
+        """
+        Select the action with the associated maximum value
+        :param state: The current state of the board along with the current mark this agent represents
+        :param available_actions: A list of available possible actions (positions on the board to mark)
+        :return: The action with the highest value
+        """
         import copy
 
-        max_index = 0
-        max_value = 0
+        max_index: int = 0
+        max_value: int = 0
         for index, action in enumerate(available_actions):
-            next_state = copy.deepcopy(state)[action]
-            next_value = self.state_values[next_state].value
+            next_state: int = copy.deepcopy(state)[action]
+            next_value: float = self.state_values[next_state].value
 
             if next_value > max_value:
-                max_index = index
-                max_value = next_value
+                max_index: int = index
+                max_value: int = next_value
 
         return available_actions[max_index]
 
-    def learn(self, state, previous_state, reward):
+    def learn(self, state: Tuple[Mark], previous_state: Tuple[Mark], reward: int):
         """
-        Apply temporal difference learning
+        Apply temporal difference learning and update the state and values of this agent
+        :param state: The current state of the board along with the current mark this agent represents
+        :param previous_state: The previous state of the board along with the current mark this agent represents
+        :param reward: The reward having taken the most recent action
         """
         self.state_values[previous_state].value += self.learning_rate * (
                 reward -
@@ -75,25 +111,36 @@ class TemporalDifference(Agent):
     def merge(self, agent):
         """
         Merge state values of another TD agent with this one
+        :param agent: another temporal difference agent
         """
         for key, value in agent.state_values.items():
             self.state_values[key] += value
 
     @staticmethod
-    def load_state_values(datafile):
-        data = pd.read_csv(datafile)
-        data = data.replace({pd.np.nan: None}).values.tolist()
+    def load_state_values(datafile: str) -> defaultdict[State]:
+        """
+        Load data from csv file and convert it to state values
+        :param datafile: The name of a csv file containing the data
+        :return: The state value mapping
+        """
+        data = pandas.read_csv(datafile)
+        data = data.replace({pandas.np.nan: None}).values.tolist()
         state_values = defaultdict(State)
         for d in data:
-            state_values[tuple(d[1:11])] = State(d[11], d[12])
+            state_values[tuple(d[1:11])] = State(value=d[11], count=d[12])
         return state_values
 
     @staticmethod
-    def save_state_values(state_values, datafile):
-        data = []
+    def save_state_values(state_values: defaultdict[State], datafile: str):
+        """
+        Save the state values into a csv file
+        :param state_values: The state value mapping
+        :param datafile: The name of the file to write to
+        """
+        data: List[Tuple[Any, ...]] = []
         for key, value in state_values.items():
             if isinstance(key, tuple):
                 data.append(key + (value.value, value.count))
 
-        df = pd.DataFrame(data)
+        df = pandas.DataFrame(data)
         df.to_csv(datafile)
